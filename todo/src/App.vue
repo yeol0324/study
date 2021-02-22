af112b342c9b0f515bdc5636dad05251d3c10712
yurim <yeol0324@pubple.com>
2021-02-22 17:46:39 +0900

    add todo, delet todo

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1 class="text-center">Todo App</h1>
+    <input type="text" @keyup.enter="addTodo" v-model="todoText" class="w-100 p-2" placeholder="Type todo" />
+    <hr>
+    <Todo v-for="todo in todos" :key="todo.id" :todo="todo" @toggle-checkbox="toggleCheckbox" @click-delete="deleteTodo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from '@/components/Todo.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    Todo
+  },
+  data() {
+    return {
+      todoText: '',
+      todos: [
+        { id: 1, text: 'learn vue', checked: false },
+        { id: 2, text: 'play game', checked: false }
+      ]
+    }
+  },
+  methods: {
+    addTodo(e) {
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false
+      });
+      this.todoText = '';
+    },
+    toggleCheckbox({id, checked}) {
+      // 구조분해 {}를 통해 해당 값에 바로 접근 가능
+      const index = this.todos.findIndex(todo => {
+        return todo.id === id;
+      });
+      this.todos[index].checked = checked;
+    },
+    deleteTodo(id) {
+      // const index = this.todos.findIndex(todo => {
+      //   return todo.id === id;
+      // });
+      // this.todos.splice(index, 1);
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
+  },  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
