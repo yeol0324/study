d13eecf830c94ca2aca9c448d3b398f555331cb7
yurim <yeol0324@gmail.com>
2021-02-23 16:57:13 +0900

    vuex

---
+++ vuextodo/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" class="container">
+    <h1>Todo App</h1>
+    <input type="text" placeholder="text">
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  data() {
+    todos: [
+      { id: 1, text: 'play game', checked: false },
+      { id: 2, text: 'go to bed', checked: false }
+    ]
+  }
+}
+</script>
