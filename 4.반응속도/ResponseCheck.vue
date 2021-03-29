d04a540f0d4885aec22b5ed6ef61d1c9cb8155d8
yurim <yeol0324@gmail.com>
2021-03-29 14:50:26 +0900

    [vue] 숫자야구

---
+++ 4.반응속도/ResponseCheck.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div id="screen">{{mesage}}</div>
+        <div>
+            <div>평균 시간: {{}}</div>
+            <button @click="onReset">리셋</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return { 
+            resault: [],
+            state: 'waiting',
+            message: '클릭해서 시작'
+        }        
+    },
+    methods: {
+        
+    }
+};
+</script>
+
+<style>
+
+</style>
---
+++ 4.반응속도/webpack.config.js
@@ -0,0 +1,29 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js','.vue']
+    },
+    entry: {
+        app: './main.js', //app > 하나로 합쳐질 파일의 이름 path.join(__dirname, 'app.js') 로 사용 가능
+    },
+    module: {//웹팩의 핵심
+        rules: [{ //수백개 수천개의 자바스크립트 파일을 합칠 때 어떻게 처리할 건지 입력
+            test: /\.vue$/,
+            loader: 'vue-loader',
+        }],
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+
+    ],
+    output: {
+        filename: 'app.js', //'[name].js'로 입력해도 무관
+        path: path.join(__dirname, 'dist'), //이 폴더에 js 파일이 생성됨
+    }
+};
+//웹패킹할 때 모듈로 만들어 놓은 객체가 사용됨, 웹팩 설정 다 여기에 입력하면 됨
+//웹팩을 사용하는 이유 : 스크립트가 너무 길어져서 하나로 합치기 위해서 사용
