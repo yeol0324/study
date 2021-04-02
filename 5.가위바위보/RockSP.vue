4493e886692cce7cb0c631138235530257849d3b
yurim <yeol0324@gmail.com>
2021-04-02 16:39:37 +0900

    [vue] 가위바위보 완성

--- 5.가위바위보/RockSP.vue
+++ 5.가위바위보/RockSP.vue
@@ -22,6 +22,17 @@ const rspCoords = {
     가위: '-142px',
     보: '-284px',
 };
+const scores = {
+    가위: 1,
+    바위: 0,
+    보: -1,
+};
+const computerChoice = (imgCoord) => {
+    return Object.entries(rspCoords).find(function(v) {
+        return v[1] === imgCoord
+    })[0];
+};
+
 let interval = null;
 export default {
     data() {
@@ -37,8 +48,35 @@ export default {
 
     },
     methods: {
+            changeHand() {
+                interval = setInterval(() => {
+                    if(this.imgCoord === rspCoords.바위 ) {
+                        this.imgCoord = rspCoords.가위;
+                    }else if(this.imgCoord === rspCoords.가위 ) {
+                        this.imgCoord = rspCoords.보;
+                    }else if(this.imgCoord === rspCoords.보 ) {
+                        this.imgCoord = rspCoords.바위;
+                    }
+                }, 100);
+            },
             onClickButton(choice) {
-                //인수를 넣고 매개변수로 받는 행위가 가능            
+                //인수를 넣고 매개변수로 받는 행위가 가능 
+                clearInterval(interval);
+                const myScore = scores[choice];
+                const cpuScore = scores[computerChoice(this.imgCoord)];
+                const diff = myScore - cpuScore;
+                if( diff === 0) {
+                    this.result = '비김';
+                }else if([-1, 2].includes(diff)) {
+                    this.result = '이김';
+                    this.score += 1;
+                }else{
+                    this.result = '짐';
+                    this.score -= 1;
+                };
+                setTimeout(() => {
+                    this.changeHand();
+                }, 1000)
             },
         },
     beforeCreate() {
@@ -54,15 +92,7 @@ export default {
     mounted() {
         console.log('mounted');
         // 화면에 나타난 후 화면까지 존재
-        interval = setInterval(() => {
-            if(this.imgCoord === rspCoords.바위 ) {
-                this.imgCoord = rspCoords.가위;
-            }else if(this.imgCoord === rspCoords.가위 ) {
-                this.imgCoord = rspCoords.보;
-            }else if(this.imgCoord === rspCoords.보 ) {
-                this.imgCoord = rspCoords.바위;
-            }
-        }, 100);
+        this.changeHand();
         // clearInterval로 멈추기 위해서 interval 변수에 저장
     },
     beforeUpdate() {
