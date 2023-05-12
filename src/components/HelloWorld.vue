cfda66221591aa0640596150cfee9b4ae26b3479
yeol0324 <73329772+yeol0324@users.noreply.github.com>
2023-05-12 18:28:01 +0900

    .

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@
 
 <script setup>
 import { onMounted, onUnmounted, ref } from "vue";
-import * as faceapi from "face-api.js";
+import * as faceapi from "@vladmandic/face-api";
 let myReq;
 let timerVar;
 const leftEyeClose = ref(0);
@@ -35,21 +35,16 @@ const eyeOut = ref(0);
 const reqTime = ref(true);
 const video = ref("video");
 const canvas = ref("canvas");
-const displaySize = {
-  width: 300,
-  height: 480,
+
+const loadModels = async () => {
+  console.log("loadmodels");
+  await Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+  ]);
 };
 onMounted(async () => {
-  const loadModels = async () => {
-    console.log(faceapi);
-    await Promise.all([
-      faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
-      faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
-      // faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
-      // faceapi.nets.faceExpressionNet.loadFromUri("./models"),
-    ]);
-  };
-
+  await loadModels();
   const startVideo = () => {
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: false })
@@ -57,6 +52,7 @@ onMounted(async () => {
         video.value.srcObject = stream;
         video.value.onloadedmetadata = function (e) {
           video.value.play();
+          alert("play");
         };
       })
       .catch(function (err) {
@@ -68,13 +64,17 @@ onMounted(async () => {
     video.value.addEventListener("play", recognize);
   });
 });
-// const cancelAnimationFrame = alert();
 const recognize = async () => {
+  console.log(faceapi);
   const detections = await faceapi
     .detectAllFaces(video.value, new faceapi.TinyFaceDetectorOptions())
-    .withFaceLandmarks(); // 랜드마크 추가
+    .withFaceLandmarks();
+  // 랜드마크 추가
   // .withFaceDescriptors(); // 안면 인식 프레임 인식된 얼굴에 주석을 설정
   // .withFaceExpressions() // 감정 분석
+
+  console.log(detections);
+  // console.log(detections.withFaceLandmarks());
   if (eyeOut.value >= 3) {
     alert("화면 안봄");
     eyeOut.value = 0;
@@ -85,25 +85,19 @@ const recognize = async () => {
   }
   detections.forEach((detection) => {
     // 랜드마크를 사용하여 양쪽 눈에 대한 포인트 추출
-    const landmarks = detection.landmarks;
-    const leftEye = landmarks.getLeftEye();
-    const rightEye = landmarks.getRightEye();
-    // 각 눈의 세로축 길이 계산
-    const leftEyeCloses =
-      (leftEye[4].y - leftEye[1].y + (leftEye[5].y - leftEye[2].y)) / 2;
-    const rightEyeCloses =
-      (rightEye[4].y - rightEye[1].y + (rightEye[5].y - rightEye[2].y)) / 2;
-    leftEyeClose.value = leftEyeCloses;
-    rightEyeClose.value = rightEyeCloses;
+    console.log(detection);
+    // const landmarks = detection.landmarks;
+    // const leftEye = landmarks.getLeftEye();
+    // const rightEye = landmarks.getRightEye();
+    // // 각 눈의 세로축 길이 계산
+    // const leftEyeCloses =
+    //   (leftEye[4].y - leftEye[1].y + (leftEye[5].y - leftEye[2].y)) / 2;
+    // const rightEyeCloses =
+    //   (rightEye[4].y - rightEye[1].y + (rightEye[5].y - rightEye[2].y)) / 2;
+    // leftEyeClose.value = leftEyeCloses;
+    // rightEyeClose.value = rightEyeCloses;
   });
 
-  // canvas 사이즈를 맞춤
-  faceapi.matchDimensions(canvas.value, displaySize);
-  const resizedDetections = faceapi.resizeResults(detections, displaySize);
-  canvas.value
-    .getContext("2d")
-    .clearRect(0, 0, canvas.value.width, canvas.value.height);
-  faceapi.draw.drawFaceLandmarks(canvas.value, resizedDetections);
   if (!reqTime.value) {
     clearTimeout(timerVar);
     return;
